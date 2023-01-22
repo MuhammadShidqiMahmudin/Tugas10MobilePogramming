8458ee31d12812de5b54e52505485f0975dc7b60
Muhammad Shidqi Mahmudin <shidqimuhammad91@gmail.com>
2023-01-22 20:10:18 +0700

    tugas 10

---
+++ NingalMovie10/app/src/main/java/com/shidqi/ningalmovie10/utils/DivMovieCallback.kt
@@ -0,0 +1,16 @@
+package  com.shidqi.ningalmovie10
+
+import androidx.recyclerview.widget.DiffUtil
+import  com.shidqi.ningalmovie10.data.model.MovieResponse
+
+class DivMovieCallback(private val oldItem: List<MovieResponse>, private val newItem: List<MovieResponse>): DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldItem.size
+
+    override fun getNewListSize(): Int = newItem.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldItem[oldItemPosition].id == newItem[newItemPosition].id
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldItem[oldItemPosition].id == newItem[newItemPosition].id
+}
